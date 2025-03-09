d9c36a663c83ca037007d2e6bfd8e659d17b69ea
felipeclarindo <fglpc2@gmail.com>
2025-03-09 01:45:01 -0300

    feat(CategoryController): Start development of the methods.

--- src/main/java/com/example/finance_walk_api/controller/CategoryController.java
+++ src/main/java/com/example/finance_walk_api/controller/CategoryController.java
@@ -1,22 +1,45 @@
 package com.example.finance_walk_api.controller;
 
-import com.example.finance_walk_api.model.Category;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.finance_walk_api.model.Category;
 
 @RestController
 public class CategoryController {
 
-    @GetMapping("/categories")
-    public List<Category> listAll(){
-        return List.of(
-                new Category(1L, "Education", "book"),
-                new Category(1L, "Education", "book"),
-                new Category(1L, "Education", "book")
+    private List<Category> repository = new ArrayList<>();
 
-        );
+    @GetMapping("/categories")
+    public List<Category> getAll() {
+        return repository;
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        repository.add(category);
+        System.out.println("Adding Category: " + category.getName());
+        return ResponseEntity.status(201).body(category);
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> getById(@PathVariable Long id) {
+        System.out.println("Getting category that id is: " + id);
+        var category = repository.stream()
+            .filter(c -> c.getId().equals(id))
+            .findFirst();
+
+        if (category.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(category.get());
     }
 }
